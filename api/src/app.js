4a05ba77152cef6c2d698126b4847895ea990e60
Eduardo <eduardoabelardo@hotmail.com>
2022-03-18 16:38:15 -0300

    Terminado

--- api/src/app.js
+++ api/src/app.js
@@ -7,9 +7,11 @@ const routes = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
+const cors = require('cors');
 
 server.name = 'API';
 
+server.use(cors());
 server.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 server.use(bodyParser.json({limit: '50mb'}));
 server.use(cookieParser());
@@ -26,7 +28,6 @@ server.use((req, res, next) => {
 });
 
 server.use('/', routes);
-const cors = require('cors');
 
 // Error catching endware.
 server.use((err, req, res, next) => {
